fcff6dd7a894ec2faf5958e5a917ef998461715d
Sanhaku <707864430@qq.com>
2022-12-21 10:35:12 +0800

    Defines the parameter types of the interfaces

--- src/certificate.py
+++ src/certificate.py
@@ -1,8 +1,8 @@
 class Certificate(object):
-    def __init__(self, gas_limit = 30_000_000):
+    def __init__(self, gas_limit=30_000_000, sequence=None):
         self.gas_limit = gas_limit
         self.squence = None
-        self.is_vulnerable = False
+        self.is_vulnerable = False if sequence is None else True
 
     def __str__(self) -> str:
         pass
--- src/gas_estimate/__init__.py
+++ src/gas_estimate/__init__.py
@@ -1,3 +1,9 @@
-def cal_iterate_times(functions_with_loop, loops, gas_limit) -> dict:
+from collections import defaultdict
+from src.cfg.loop import Loop
+
+import src.rattle as rattle
+
+
+def cal_iterate_times(functions_with_loop: defaultdict(rattle.SSAFunction), loops: defaultdict(list(Loop)), gas_limit=30_000_000) -> dict:
     # TODO: calculatre iterate_times for every loop
     pass
