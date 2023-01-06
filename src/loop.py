7199905a688b1403a2357148848b86057d6c3817
Sanhaku <707864430@qq.com>
2023-01-06 12:45:23 +0800

    find key variable in loop

--- src/loop.py
+++ src/loop.py
@@ -1,4 +1,8 @@
+import logging
 from src.rattle import Recover
+from src.utils import get_source
+
+logger = logging.getLogger(__name__)
 
 
 class Loop(object):
@@ -8,7 +12,7 @@ class Loop(object):
         self.body = []
         self.back_edges = []
         self.bbs = []
-        self.key_value = None
+        self._key_variable = None
         self._iterate_times = 0
         self._tx_sequence = None
 
@@ -35,6 +39,39 @@ class Loop(object):
     def tx_sequence(self, value):
         self._tx_sequence = value
 
+    @property
+    def key_variable(self):
+        return self._key_variable
+
+    @key_variable.setter
+    def key_variable(self, value):
+        self._key_variable = value
+
     def find_key_variable(self, ssa: Recover):
         # TODO: find key variable for the loop
-        pass
+
+        for f in ssa.functions:
+            for b in f.blocks:
+                if b.offset == self.head:
+                    head_block = b
+                    break
+
+        if len(head_block.insns) < 2 or head_block.insns[-2].insn.name != 'ISZERO':
+            return
+        judge_arg = head_block.insns[-2].arguments[0]
+
+        writer_insn = judge_arg.writer
+        source = []
+        get_source(writer_insn, source, [])
+
+        if len(source) == 0:
+            logger.warning("Cannot find key variable")
+            return
+
+        self._key_variable = source
+        # self._key_variable = []
+        # for s in source:
+        #     if s.insn.name == 'CALLDATALOAD':
+        #         self._key_variable.append(('CALLDATALOAD', s.arguments[0]))
+        #     elif s.insn.name == 'SLOAD':
+        #         self._key_variable.append(('SLOAD', s.arguments[0]))
